5849004f824cb2a160650ac76637cc49015fb185
Kyle Garza <kyle.garza@yahoo.com>
2018-10-04 11:27:19 -0500

    Added a vertex class for BFS

---
+++ Vertex.java
@@ -0,0 +1,34 @@
+/* WORD LADDER Vertex.java
+ * EE422C Project 3 submission by
+ * <Jessica Heerboth>
+ * <jrh5728>
+ * <16355>
+ * <Kyle Garza>
+ * <kcg568>
+ * <16355>
+ * Slip days used: <0>
+ * Git URL: https://github.com/EE422C-Fall-2018/project-3-bfs-dfs-pair-45.git
+ * Fall 2018
+ */
+
+package assignment3;
+
+import java.util.*;
+
+public class Vertex {
+	public int index;
+	private boolean discovered;
+	private Vertex parent = null;
+	private List<Vertex> adj = new LinkedList<Vertex>();
+
+	public void addEdge(Vertex v) {
+		adj.add(v);
+	}
+}
+
+class Graph {
+	List<Vertex> graph;
+
+	Queue<Vertex> q = new LinkedList<Vertex>();
+
+}
